3f7b9e7cbab02cc8ca3d41d0354fa0e70d70d336
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2023-09-12 15:16:47 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
